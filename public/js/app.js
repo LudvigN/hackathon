d8d9c8b29d875be6e20513c22ff7887859ab14ea
Ludvig Nilson <ludvig@ludvig-system.(none)>
2014-06-07 19:44:45 +0200

    Cikel exempel

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,14 @@
 var hello = (function(){
 	document.write('<h1>Hello World</h1>');
 })();
+
+
+var svg = d3.select("body").append("svg")
+			.attr("width", 200)
+			.attr("width", 700);
+
+
+svg.append("circle")
+	.attr("r",40)
+	.attr("cx",40)
+	.attr("cy",40);
